6fc531e06bb7d23f07aeea42137d2b6cda0555c6
Jethro Yu <jethro@teamt5.org>
2017-12-08 14:23:56 +0800

    Add dependency on cython

--- setup.py
+++ setup.py
@@ -13,23 +13,53 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from codecs import open
+from os import path
 
-def config():
-    import meliae
-    ext = []
-    kwargs = {
-        "name": "meliae",
-        "version": meliae.__version__,
-        "description": "Python Memory Usage Analyzer",
-        "author": "John Arbash Meinel",
-        "author_email": "john.meinel@canonical.com",
-        "url": "https://launchpad.net/meliae",
-        "license": "GNU GPL v3",
-        "download_url": "https://launchpad.net/meliae/+download",
-        "packages": ["meliae"],
-        "scripts": ["strip_duplicates.py"],
-        "ext_modules": ext,
-        "classifiers": [
+from setuptools import Extension, find_packages, setup
+
+import meliae
+
+here = path.abspath(path.dirname(__file__))
+
+extensions = [
+    Extension("meliae._scanner", ["meliae/_scanner.pyx", "meliae/_scanner_core.c"]),
+    Extension("meliae._loader", ["meliae/_loader.pyx"]),
+    Extension("meliae._intset", ["meliae/_intset.pyx"]),
+]
+
+try:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+except ImportError:
+    pass
+
+setup(
+    name='meliae',
+    version=meliae.__version__,
+    description="Python Memory Usage Analyzer",
+    long_description="""\
+                    This project is similar to heapy (in the 'guppy' project), in its attempt to
+                    understand how memory has been allocated.
+
+                    Currently, its main difference is that it splits the task of computing summary
+                    statistics, etc of memory consumption from the actual scanning of memory
+                    consumption. It does this, because I often want to figure out what is going on
+                    in my process, while my process is consuming huge amounts of memory (1GB, etc).
+                    It also allows dramatically simplifying the scanner, as I don't allocate python
+                    objects while trying to analyze python object memory consumption.
+
+                    It will likely grow to include a GUI for browsing the reference graph. For now
+                    it is mostly used in the python interpreter.
+
+                    The name is simply a fun word (means Ash-wood Nymph).
+                    """,
+    url='https://launchpad.net/meliae',
+    author='John Arbash Meinel',
+    author_email='john.meinel@canonical.com',
+    license='GNU GPL v3',
+    download_url="https://launchpad.net/meliae/+download",
+    classifiers=[
             'Development Status :: 4 - Beta',
             'Environment :: Console',
             'Intended Audience :: Developers',
@@ -40,43 +70,11 @@ def config():
             'Programming Language :: Python',
             'Programming Language :: Cython',
             'Topic :: Software Development :: Debuggers',
-        ],
-        "long_description": """\
-This project is similar to heapy (in the 'guppy' project), in its attempt to
-understand how memory has been allocated.
-
-Currently, its main difference is that it splits the task of computing summary
-statistics, etc of memory consumption from the actual scanning of memory
-consumption. It does this, because I often want to figure out what is going on
-in my process, while my process is consuming huge amounts of memory (1GB, etc).
-It also allows dramatically simplifying the scanner, as I don't allocate python
-objects while trying to analyze python object memory consumption.
-
-It will likely grow to include a GUI for browsing the reference graph. For now
-it is mostly used in the python interpreter.
-
-The name is simply a fun word (means Ash-wood Nymph). 
-"""
-    }
-
-    from distutils.core import setup, Extension
-
-    try:
-        from Cython.Distutils import build_ext
-    except ImportError:
-        print "We require Cython to be installed."
-        return
-
-    kwargs["cmdclass"] = {"build_ext": build_ext}
-    ext.append(Extension("meliae._scanner",
-                         ["meliae/_scanner.pyx",
-                          "meliae/_scanner_core.c"]))
-    ext.append(Extension("meliae._loader",
-                         ["meliae/_loader.pyx"]))
-    ext.append(Extension("meliae._intset",
-                         ["meliae/_intset.pyx"]))
-
-    setup(**kwargs)
-
-if __name__ == "__main__":
-    config()
+    ],
+    keywords='memory consumption profile',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    scripts=['strip_duplicates.py'],
+    install_requires=['cython'],
+    setup_requires=['cython'],
+    ext_modules=extensions,
+)
